8297174f2845bb9030b8d7e8e9426fa903b8f52b
Katrina Dierking <kalynndier@gmail.com>
2021-05-18 15:14:37 -0400

    users, posts, and validation created

---
+++ index.js
@@ -0,0 +1,29 @@
+//dependency imports
+const {ApolloServer} = require('apollo-server');
+const mongoose = require('mongoose');
+
+//relative imports
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const {MONGODB} = require('./config');
+
+
+
+const server = new ApolloServer({
+    typeDefs, 
+    resolvers
+});
+
+
+mongoose
+    .connect(MONGODB, {useNewUrlParser: true} )
+    .then(() => {
+        console.log('MongoDB connected');
+        return server.listen({ port: 5000 })
+    })
+    .then(res => {
+        console.log(`Server running at ${res.url}`)
+    })
+    .catch(err => {
+        console.error(err)
+    })
